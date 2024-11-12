6b947386a5a2118adb84adaa7d7227b8175dcc65
mpii <kelvianov10@gmail.com>
2024-11-12 14:45:35 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,9 @@
 <div class="w-100">
 
     <main class="form-signin w-100 m-auto">
+
         <form method="POST" action="{{ route('auth.login') }}" id="login-form">
+            @csrf
             <h1 class="h3 mb-3 fw-normal">Silahkan masuk untuk absensi</h1>
 
             <div class="form-floating">
